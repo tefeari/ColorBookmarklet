80ce51675ebdd60b148878f7e64708409fb20c34
David Holz <davidsholz@gmail.com>
2019-12-06 21:46:48 -0800

    Add reduced colorspace options

---
+++ bk_class_sat_3_100_01.js
@@ -0,0 +1,15 @@
+// Add this as your bookmarklet:
+// javascript:{var s=document.createElement("script");s.src="https://zalo.github.io/assets/js/TextColoring/bookmarklet.js",document.body.appendChild(s);};void(0);
+
+// Execute on a delay in-case the body hasn't been constructed yet...
+setTimeout(function() {
+    var i, s, ss = [
+        'https://cdnjs.cloudflare.com/ajax/libs/findAndReplaceDOMText/0.4.6/findAndReplaceDOMText.min.js',
+        'https://tefeari.github.io/ColorBookmarklet/hw_class_sat_3_100_01.js'
+    ];
+    for (i = 0; i != ss.length; i++) {
+        s = document.createElement('script');
+        s.src = ss[i];
+        document.body.appendChild(s);
+    }
+}, 100);
