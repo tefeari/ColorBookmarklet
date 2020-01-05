c03d4633192554314555d65e7f2ba19e6aa63f96
David Holz <davidsholz@gmail.com>
2020-01-05 15:32:15 -0800

    Freq Maps

---
+++ hw_freq_gnuplot.js
@@ -0,0 +1,24 @@
+fetch('https://tefeari.github.io/ColorBookmarklet/freq_heat_gnuplot.json').then(function(response) {
+    response.json().then(function(result) {
+        var words = document.body.textContent.split(/[^a-zA-Z]+/g);
+        var doneWords = {};
+        var curWord = "";
+        for (var i = 0; i < words.length; i++) {
+            curWord = words[i].replace(/[^A-Za-z0-9\s]/g, "").trim().toLowerCase();
+            if ((curWord.length > 0) &&
+                !(curWord in doneWords) &&
+                (curWord in result)) {
+                findAndReplaceDOMText(document.body, {
+                    find: new RegExp("\\b" + curWord + "\\b", "gi"),
+                    replace: function(portion, match) {
+                        var span = document.createElement("SPAN");
+                        span.style.color = result[curWord];
+                        span.innerText = portion.text;
+                        return span;
+                    }
+                });
+            }
+            doneWords[curWord] = true;
+        }
+    });
+});
